f390f3a9973863519de55ed3cc14c67bc260b9d7
LukeBenson <luke_benson@outlook.com>
2019-11-01 10:49:34 +0000

    new file added for flask scripts only

---
+++ flask-app/flask-service-local.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+git clone https://github.com/LukeBenson/Individual-Project.git
+cd Individual-Project/IP_Flask
+
+. ./application/venv/bin/activate
+
+pip3 install -r requirements.txt
+
+python3 ./run.py
