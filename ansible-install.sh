6f4f7b36b8c85878b570f24316c94c1c1e72341c
LukeBenson <luke_benson@outlook.com>
2020-02-13 11:37:34 +0000

    amending ansible script

--- ansible-install.sh
+++ ansible-install.sh
@@ -4,11 +4,12 @@
 if [[ "$VIRTUAL_ENV" != "" ]]; then
 	echo "You are working in a virtual environment so Ansible will not be able to work with the apt package manager."
 	echo "Please deactivate your virtual environment and re-run the install script."
-	exit
+	return
 fi
 
 # Ensure ~/.local/bin exists and is on the PATH.
-if [[ ":$PATH:" != ":/home/$USER/.local/bin" ]]; then
+if [[ ":$PATH:" != "*:/home/$USER/.local/bin:*" ]]; then
+	echo "Adding ~/.local/bin to path."
 	echo "PATH=$PATH:/home/$USER/.local/bin" >> ~/.bashrc
 	source /home/$USER/.bashrc
 fi
