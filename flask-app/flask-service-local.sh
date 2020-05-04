19902c0b463ec11a823b70a2093c7b71ef135b92
LukeBenson <luke_benson@outlook.com>
2020-05-04 15:24:44 +0100

    removing flaskapp install scripts as they are now obselete

--- flask-app/flask-service-local.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-git clone https://github.com/LukeBenson/Individual-Project.git
-cd Individual-Project/IP_Flask
-
-. ./application/venv/bin/activate
-
-pip3 install -r requirements.txt
-
-python3 ./run.py
