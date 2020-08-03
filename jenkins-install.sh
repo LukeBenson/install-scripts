abda34180003511940880dbca2ebf17ea60e3d5f
LukeBenson <luke_benson@outlook.com>
2020-08-03 08:33:02 +0000

    making install more readable

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -3,18 +3,26 @@ if type apt > /dev/null; then
     pkg_mgr=apt
     java="default-jdk-headless"
 fi
+
 echo "installing dependencies"
+sleep 2
 sudo ${pkg_mgr} install -y ${java}
+
 echo "Downloading GPG Key"
+sleep 2
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
 echo "Adding Repos"
+sleep 2
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo add-apt-repository universe
 
 echo "Updating package manager"
+sleep 2
 sudo apt-get update
 
 echo "Installing Jenkins"
+sleep 2
 sudo apt-get install jenkins -y
 
 sudo su - jenkins << EOF
